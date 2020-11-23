d13ae04a1b9bc2a5bf58fb243ecf8fa7b8cc44a2
a2per <72902145+a2per@users.noreply.github.com>
2020-11-23 06:41:18 -0500

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -2,16 +2,16 @@
 
 import pygame
 import random
-WIDTH = 800 
-HEIGHT = 800 
+WIDTH = 800
+HEIGHT = 800
 FPS = 30
 
 n = int(input("grid size:"))
 grid = [n][n]
 N,S,E,W = 1,2,4,8
-dx = [E==1, W==-1, N==0, S==0]
-dy = [E==0, W==0, N==-1, S==1]
-opp = [E==W, W==E, N==S, S==N]
+dx = [1, 1, 0, 0] #E,W,N,S
+dy = [0, 0, -1, 1]#E,W,N,S
+opp = [W, E, S, N]
 
 
 def getPath(x,y,g):
@@ -27,32 +27,32 @@ def getPath(x,y,g):
                     opp[d]=="W"
                     elif(d=="W"):
                         opp[d]=="E"
-            
+
         nx = x + dx[d]
         ny = y + dy[d]
         if( ((0<= ny) and (ny<=((g.length)-1)) and ((0<=nx) and (nx<=((g[ny].length)-1)) and g[ny][nx]==[0][0]):
              g[y][x].append(d)
              g[ny][nx].append(opp[d])
              getPath(nx,ny,g)
-    
-# initialize Pygame 
-pygame.init() 
-pygame.mixer.init() 
-screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
-pygame.display.set_caption("Grid") 
+
+# initialize Pygame
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Grid")
 clock = pygame.time.Clock()
 
-white = [255, 255, 255] 
-black = [0,0,0] 
-screen.fill(white) 
+white = [255, 255, 255]
+black = [0,0,0]
+screen.fill(white)
 pygame.display.update()
 
 running = True
 
 
-while running: 
- # keep running at the at the right speed 
- clock.tick(FPS) 
+while running:
+ # keep running at the at the right speed
+ clock.tick(FPS)
  # process input (events)
  getPath(0,0,grid)
 
@@ -71,9 +71,9 @@ while running:
                   pygame.draw.line()
         else:
           print "|"
- 
- 
- for event in pygame.event.get(): 
- # check for closing the window 
-     if (event.type == pygame.QUIT): 
+
+
+ for event in pygame.event.get():
+ # check for closing the window
+     if (event.type == pygame.QUIT):
          running = False
